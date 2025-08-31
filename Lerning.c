3ddab0ad9f9fa6023d60d63333d0264580cd6b25
TarasBilyk1 <hinaataras@gmail.com>
2025-08-31 19:51:04 +0300

    Оновив файл Lerning.c з новим кодом

--- Lerning.c
+++ Lerning.c
@@ -1,20 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-void main() {
+ void fifty_fifty(int array_random[],int arry_size)
+{
+	 for (int i = 0; i < arry_size; i++)
+		 array_random[i] = rand() %100 + 1;
+}
 
-	int array[10] ;
-	
-	for (int i = 0; i < 10; i++) {
-		scanf_s("%d", &array[i]);
-	}
-	int max = array[0];
+int main(void) {
+	srand(time(NULL));
 
-	for (int i = 0; i < 10; i++) {
-		if (array[i] > max) {
-			max = array[i];
+	int Array_NUM[8];
+
+	fifty_fifty(Array_NUM,8);
+
+	int max = Array_NUM[0];
+	for (int i = 0; i < 8; i++) {
+		if (Array_NUM[i] > max) {
+			max = Array_NUM[i];
 		}
 	}
 	printf(" Your max number in the array = %d \n", max);
+	printf(" \n ");
+
+	for (int i = 0; i < 8; i++) {
+		printf("Arr = %d ",Array_NUM[i]); 
+	}
+	printf(" \n ");
 
 	return 0;
-}   
+}

c295c15234129e88a8f767ecaff3d473ee55497f
TarasBilyk1 <hinaataras@gmail.com>
2025-08-31 21:54:21 +0300

    add info  Добавив гру з вгадуванням числа !

--- Lerning.c
+++ Lerning.c
@@ -2,18 +2,17 @@
 #include <stdlib.h>
 #include <time.h>
 
- void fifty_fifty(int array_random[],int arry_size)
-{
-	 for (int i = 0; i < arry_size; i++)
-		 array_random[i] = rand() %100 + 1;
-}
+void fifty_fifty(int array_random[], int arry_size);
 
 int main(void) {
 	srand(time(NULL));
 
 	int Array_NUM[8];
+	int arry_for_guess[3];
 
-	fifty_fifty(Array_NUM,8);
+	printf(" Guess a number from 1 to 100 : ");
+	fifty_fifty( Array_NUM,8);
+	scanf_s("%d %d %d", &arry_for_guess[0], &arry_for_guess[1], &arry_for_guess[2]);
 
 	int max = Array_NUM[0];
 	for (int i = 0; i < 8; i++) {
@@ -21,13 +20,23 @@ int main(void) {
 			max = Array_NUM[i];
 		}
 	}
-	printf(" Your max number in the array = %d \n", max);
-	printf(" \n ");
 
-	for (int i = 0; i < 8; i++) {
-		printf("Arr = %d ",Array_NUM[i]); 
+	printf(" Your max number in the array = %d \n", max  );
+
+	if (arry_for_guess[0] == max || arry_for_guess[1] == max || arry_for_guess[2] == max) {
+		printf(" You Win !) Arr =  ");
 	}
-	printf(" \n ");
+	else {
+		printf(" You loss !( Arr =  ");
+	}
+	for (int i = 0; i < 8; i++)
+			printf(" %d , ", Array_NUM[i]);
 
 	return 0;
+}
+
+void fifty_fifty(int array_random[], int arry_size)
+{
+	for (int i = 0; i < arry_size; i++)
+		array_random[i] = rand() % 100 + 1;
 }

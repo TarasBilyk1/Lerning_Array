1649f5221925f1ee7febaefabaacc7593c6e57c1
TarasBilyk1 <hinaataras@gmail.com>
2025-09-08 20:54:24 +0300

    Зробив функцію ще потребує дороблення

--- Lerning.c
+++ Lerning.c
@@ -2,8 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
+#include <setjmp.h>
+
+jmp_buf buf_memory;
 
 void fifty_fifty(int array_random[], int arry_size);
+void return_to_Writhe_Fan(int arry_for_guess[], int arry_size);
+void throw_exc(error_code);
 
 int main(void) {
     srand((unsigned int)time(NULL));
@@ -15,6 +20,7 @@ int main(void) {
     short loss = 0;
     short game = 0;
     bool Stop = false;
+    bool ST = false;
 
     FILE* file;
     errno_t err = fopen_s(&file, "stats.txt", "a");
@@ -29,7 +35,17 @@ int main(void) {
 
         printf("Guess a number from 1 to 100: ");
         fifty_fifty(Array_NUM, 8);
-        scanf_s("%d %d %d", &arry_for_guess[0], &arry_for_guess[1], &arry_for_guess[2]);
+
+        // Тут зараз головна біль 
+        do {
+            scanf_s("%d %d %d", &arry_for_guess[0], &arry_for_guess[1], &arry_for_guess[2]);
+
+            int return_to_write = setjmp(buf_memory);
+            if (return_to_write == 0) {  
+                return_to_Writhe_Fan(arry_for_guess,3);
+            }
+
+        } while (!ST);
 
         int max = Array_NUM[0];
         for (short i = 0; i < 8; i++) {
@@ -74,7 +90,23 @@ int main(void) {
     return 0;
 }
 
+// Функація випаткового вводу чисел в масив 
 void fifty_fifty(int array_random[], int arry_size) {
     for (short i = 0; i < arry_size; i++)
         array_random[i] = rand() % 100 + 1;
+}
+// Функія знаходження виннятку
+
+// Доробити цю функцію 
+void return_to_Writhe_Fan(int arry_for_guess[], int arry_size) {
+    for (short i; i< arry_size ;i++ ) {
+        if (i < 1 && i> 100) {
+
+            throw_exc(1);
+        }
+    }
+}
+
+void throw_exc(int error_code) {
+    longjmp(buf_memory, error_code);
 }
